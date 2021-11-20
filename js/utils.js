34a92b260b331e2514d598cbc6266ed530e2ca2e
fontilan <68748054+fontilan@users.noreply.github.com>
2021-11-20 15:50:25 +0100

    change isLoyalty from boolean to enum

--- js/utils.js
+++ js/utils.js
@@ -4,8 +4,9 @@ exports.populateUser = exports.showReviewTotal = void 0;
 var returningUserDisplay = document.querySelector("#returning-user");
 var userNameDisplay = document.querySelector("#user");
 var reviewTotalDisplay = document.querySelector("#reviews");
+var enums_js_1 = require("./enums.js");
 function showReviewTotal(value, reviewer, isLoyalty) {
-    var iconDisplay = isLoyalty ? "⭐" : "";
+    var iconDisplay = isLoyalty === enums_js_1.LoyaltyUser.GOLD_USER ? "⭐" : "";
     reviewTotalDisplay.innerHTML = "\n    total reviews: " + value.toString() + "\n    |\n    last reviewed by " + reviewer + "\n    " + iconDisplay + "\n  ";
 }
 exports.showReviewTotal = showReviewTotal;
