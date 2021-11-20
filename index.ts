edc25f64c887d706fdf985ed928016c75baf205a
fontilan <68748054+fontilan@users.noreply.github.com>
2021-11-20 15:52:12 +0100

    added tuples and enums

--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
-// import { showReviewTotal, populateUser } from "./utils";
+// import { showReviewTotal, populateUser } from "./utils.js";
+// import { Permissiones, LoyaltyUser } from "./enums.js";
+
+//
+//
+//
+// MOVE THIS TO A SEPARATE FILE
+//
+
 const returningUserDisplay = document.querySelector("#returning-user");
 const userNameDisplay = document.querySelector("#user");
 const reviewTotalDisplay = document.querySelector("#reviews");
-const propertiesEl = document.querySelector(".properties");
 
-function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
-  const iconDisplay = isLoyalty ? "⭐" : "";
+function showReviewTotal(value: number, reviewer: string, isLoyalty: string) {
+  const iconDisplay = isLoyalty === LoyaltyUser.GOLD_USER ? "⭐" : "";
   reviewTotalDisplay!.innerHTML = `
     total reviews: ${value.toString()}
     |
@@ -22,9 +29,33 @@ function populateUser(isReturning: boolean, userName: string) {
 }
 //
 //
-// The above can be moved to a separate file, but at the moment I don't know how :(
 //
 //
+//
+
+//
+//
+// MOVE THIS TO ANOTHER SEPARATE FILE
+//
+// //
+enum Permissiones {
+  ADMIN,
+  READ_ONLY,
+}
+
+enum LoyaltyUser {
+  GOLD_USER = "GOLD_USER",
+  SILVER_USER = "SILVER_USER",
+  BRONZE_USER = "BRONZE_USER",
+}
+//
+//
+//
+//
+//
+
+const propertiesEl = document.querySelector(".properties");
+const footerEl = document.querySelector(".footer");
 
 let isOpen: boolean;
 
@@ -32,39 +63,43 @@ let isOpen: boolean;
 const reviews: {
   name: string;
   stars: number;
-  loyaltyUser: boolean;
+  loyaltyUser: LoyaltyUser;
   date: string;
 }[] = [
   {
     name: "Sheia",
     stars: 5,
-    loyaltyUser: true,
+    loyaltyUser: LoyaltyUser.GOLD_USER,
     date: "01-04-2021",
   },
   {
     name: "Andrzej",
     stars: 3,
-    loyaltyUser: false,
+    loyaltyUser: LoyaltyUser.BRONZE_USER,
     date: "28-03-2021",
   },
   {
     name: "Omar",
     stars: 4,
-    loyaltyUser: true,
+    loyaltyUser: LoyaltyUser.SILVER_USER,
     date: "27-03-2021",
   },
 ];
 
-// User
+const ADMIN = "admin";
+const READ_ONLY = "read-only";
+
 const you: {
   firstName: string;
   lastName: string;
+  permissions: Permissiones;
   isReturning: boolean;
   age: number;
   stayedAt: string[];
 } = {
   firstName: "Bobby",
   lastName: "Brown",
+  permissions: Permissiones.ADMIN,
   isReturning: true,
   age: 35,
   stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
@@ -83,7 +118,7 @@ const properties: {
     postcode: number;
     country: string;
   };
-  contact: string;
+  contact: [number, string];
   isAvailable: boolean;
 }[] = [
   {
@@ -96,7 +131,7 @@ const properties: {
       postcode: 45632,
       country: "Colombia",
     },
-    contact: "marywinkle@gmail.com",
+    contact: [+1123495082908, "marywinkle@gmail.com"],
     isAvailable: true,
   },
   {
@@ -109,7 +144,7 @@ const properties: {
       postcode: 34904,
       country: "Poland",
     },
-    contact: "jimdavies@hotmail.com",
+    contact: [+1123495082908, "jimdavies@hotmail.com"],
     isAvailable: false,
   },
   {
@@ -122,7 +157,7 @@ const properties: {
       postcode: 444984,
       country: "United Kingdom",
     },
-    contact: "joejones@outlook.com",
+    contact: [+1123495082908, "joejones@outlook.com"],
     isAvailable: true,
   },
 ];
@@ -142,3 +177,7 @@ for (let i = 0; i < properties.length; i++) {
   card.appendChild(image);
   propertiesEl?.appendChild(card);
 }
+
+// use your location, your current time, and current temperature of your location
+let currentLocation: [string, string, number] = ["Kraków", "15:00", 12];
+footerEl!.innerHTML = `${currentLocation[0]} ${currentLocation[1]} ${currentLocation[2]}°`;
