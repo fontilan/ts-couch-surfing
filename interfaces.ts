9909f1c5bdc7f2edc9035467398feb74959a3cf4
fontilan <68748054+fontilan@users.noreply.github.com>
2021-11-23 21:07:51 +0100

    add the Property interface

--- interfaces.ts
+++ interfaces.ts
@@ -1,12 +1,23 @@
-enum LoyaltyUser {
-  GOLD_USER = "GOLD_USER",
-  SILVER_USER = "SILVER_USER",
-  BRONZE_USER = "BRONZE_USER",
-}
+import { LoyaltyUser } from "./enums.js";
+import { Price, Country } from "./types.js";
 
-export default interface Review {
+export interface Review {
   name: string;
   stars: number;
   loyaltyUser: LoyaltyUser;
   date: string;
+}
+
+export interface Property {
+  image: string;
+  title: string;
+  price: Price;
+  location: {
+    firstLine: string;
+    city: string;
+    postcode: number | string;
+    country: Country;
+  };
+  contact: [number, string];
+  isAvailable: boolean;
 }
