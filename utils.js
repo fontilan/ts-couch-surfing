858e40be5562011f64b814712ecfb898021b4b11
fontilan <68748054+fontilan@users.noreply.github.com>
2021-11-23 21:31:47 +0100

    just a test

---
+++ utils.js
@@ -0,0 +1,37 @@
+const returningUserDisplay = document.querySelector("#returning-user");
+const userNameDisplay = document.querySelector("#user");
+const reviewTotalDisplay = document.querySelector("#reviews");
+import { LoyaltyUser } from "./enums.js";
+export function showReviewTotal(value, reviewer, isLoyalty) {
+    const iconDisplay = isLoyalty === LoyaltyUser.GOLD_USER ? "⭐" : "";
+    reviewTotalDisplay.innerHTML = `
+    ${value.toString()} review${makeMultiple(value)}
+    |
+    last reviewed by ${reviewer}
+    ${iconDisplay}
+  `;
+}
+export function populateUser(isReturning, userName) {
+    if (isReturning) {
+        returningUserDisplay.innerHTML = "back, ";
+    }
+    userNameDisplay.innerHTML += userName;
+}
+export function showDetails(value, element, price) {
+    if (value) {
+        const priceDisplay = document.createElement("div");
+        priceDisplay.innerHTML = `\$${price.toString()} / night`;
+        element.appendChild(priceDisplay);
+    }
+}
+export function makeMultiple(value) {
+    if (value > 1 || value === 0) {
+        return "s";
+    }
+    else
+        return "";
+}
+export function getTopTwoReviews(reviews) {
+    const sortedReviews = reviews.sort((a, b) => b.stars - a.stars);
+    return sortedReviews.slice(0, 2);
+}
